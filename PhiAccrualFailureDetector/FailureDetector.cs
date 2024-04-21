8dff1b05ed84deb196a68d5c238c1bb6b67df837
beyonddream <43626691+beyonddream@users.noreply.github.com>
2024-04-20 22:43:09 -0700

    make the signature using default class constructor

--- PhiAccrualFailureDetector/FailureDetector.cs
+++ PhiAccrualFailureDetector/FailureDetector.cs
@@ -5,10 +5,8 @@
 /// [https://oneofus.la/have-emacs-will-hack/files/HDY04.pdf]
 /// It is based on Akka's implementation in Scala.
 /// </summary>
-public class FailureDetector
+public class PhiAccrualFailureDetector(double threshold, int maxSampleSize, TimeSpan minStdDeviation, TimeSpan acceptableHeartBeatPause, TimeSpan firstHeartbeatEstimate) 
 {
-    public FailureDetector() {
-        
-    }
+    
 
 }
